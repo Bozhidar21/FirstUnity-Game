f7e08d58e7eb7c45f1b9b03de38aa4d865b5dc6f
Bozhidar Zhivkov <darko21479@gmail.com>
2021-09-30 19:25:39 +0300

    SpeedChange[PlayerMovementScript]

--- Project 2/Assets/Player/PlayerMovement.cs
+++ Project 2/Assets/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float speed = 20f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
